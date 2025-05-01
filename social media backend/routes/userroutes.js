79f4d016516e1a97818cb11ded2e1ba846cecff5
SriramSV7 <138434567+SriramSV7@users.noreply.github.com>
2025-05-01 18:25:51 +0530

    Delete social media backend directory

--- social media backend/routes/userroutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getUser, updateUser } = require('../controllers/userController');
-const { protect } = require('../middlewares/authMiddleware');
-const router = express.Router();
-
-router.get('/:id', protect, getUser);
-router.patch('/:id', protect, updateUser);
-
-module.exports = router;
